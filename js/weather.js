5935e2b173115d9ecd0d33310a5c5445eca493f8
ljheayeee <98166367+ljheayeee@users.noreply.github.com>
2022-03-21 01:28:28 +0900

    일단 끝

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "82ff4e5020dd831a6d9bd9fce1824d74";
+
+function onGeoOk(positon) {
+  const lat = positon.coords.latitude;
+  const lon = positon.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
